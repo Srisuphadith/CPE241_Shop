43e5a0ac6f22eef7ea7ec9a9e10d123549e8ce0e
Fubuki <75804199+Srisuphadith@users.noreply.github.com>
2025-05-11 13:00:02 +0700

    add cancel button

--- user/add_address.php
+++ user/add_address.php
@@ -90,6 +90,9 @@
     </div>
 
     <div class="flex items-center justify-between">
+      <a href="manage_address.php" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+        Cancel
+        </a>
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" name ="submit">
         confirm
       </button>
